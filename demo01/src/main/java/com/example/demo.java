cd2fb873b715fac50e9b4c65a75642c52e672801
tantj00x <140320241+tantj00x@users.noreply.github.com>
2023-08-09 19:13:07 +0800

    Delete demo01 directory

--- demo01/src/main/java/com/example/demo.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class demo {
-    public static void main(String[] args) {
-        System.out.println("测试代码的执行");
-        System.out.println("------------------------------------------------");
-    }
-}
