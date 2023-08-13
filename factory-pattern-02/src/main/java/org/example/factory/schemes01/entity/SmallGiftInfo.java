462b739cb611adfa8c611a0f5b64dbc97fb060f1
PJJ <tantj00x@gmail.com>
2023-08-13 13:48:38 +0800

    小礼品信息对应实体类

---
+++ factory-pattern-02/src/main/java/org/example/factory/schemes01/entity/SmallGiftInfo.java
@@ -0,0 +1,57 @@
+package org.example.factory.schemes01.entity;
+
+/**
+ * 小礼品信息对应实体类
+ */
+public class SmallGiftInfo {
+
+    private String userName;
+
+    private String userPhone;
+
+    private String orderId;
+
+    private String address;
+
+    public SmallGiftInfo() {
+    }
+
+    public SmallGiftInfo(String userName, String userPhone, String orderId, String address) {
+        this.userName = userName;
+        this.userPhone = userPhone;
+        this.orderId = orderId;
+        this.address = address;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
