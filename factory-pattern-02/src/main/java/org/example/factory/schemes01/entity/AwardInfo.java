c48d630b8b7c488fd6cc6160ca9191baa6bda486
PJJ <tantj00x@gmail.com>
2023-08-13 13:42:37 +0800

    获奖信息实体类

---
+++ factory-pattern-02/src/main/java/org/example/factory/schemes01/entity/AwardInfo.java
@@ -0,0 +1,71 @@
+package org.example.factory.schemes01.entity;
+
+import java.util.Map;
+
+/**
+ * 获奖信息实体类
+ */
+public class AwardInfo {
+
+    /**
+     * 用户ID
+     */
+    private String uid;
+
+    /**
+     * 奖品信息：1.打折券 2.优酷会员 3.小礼品
+     */
+    private Integer awardTypes;
+
+    /**
+     * 奖品编号
+     */
+    private String awardNumber;
+
+    /**
+     * 额外信息
+     */
+    private Map<String,String> extMap;
+
+    public AwardInfo() {
+    }
+
+    public AwardInfo(String uid, Integer awardTypes, String awardNumber, Map<String, String> extMap) {
+        this.uid = uid;
+        this.awardTypes = awardTypes;
+        this.awardNumber = awardNumber;
+        this.extMap = extMap;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public Integer getAwardTypes() {
+        return awardTypes;
+    }
+
+    public void setAwardTypes(Integer awardTypes) {
+        this.awardTypes = awardTypes;
+    }
+
+    public String getAwardNumber() {
+        return awardNumber;
+    }
+
+    public void setAwardNumber(String awardNumber) {
+        this.awardNumber = awardNumber;
+    }
+
+    public Map<String, String> getExtMap() {
+        return extMap;
+    }
+
+    public void setExtMap(Map<String, String> extMap) {
+        this.extMap = extMap;
+    }
+}
